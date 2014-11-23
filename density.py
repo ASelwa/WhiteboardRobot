b0c095f3f814e6ef2ba8e5d8badb5483bad3dabb
isaacphi <isaac.phil@gmail.com>
2014-11-23 11:01:37 -0500

    density code needs debugging

--- density.py
+++ density.py
@@ -18,12 +18,14 @@ LEFT_EDGE = 100
 RIGHT_EDGE = 1800
 TOP_EDGE = 100
 BOTTOM_EDGE = 1000
+HORIZONTAL_SPACE = 1.1
+VERT_RES = 0.2
 
 device = "/dev/ttyACM0"
 device2 = "/dev/ttyAMC1"
 
 def main():   
-    global cur_x, cur_y, scale, ser
+    global cur_x, cur_y, ser
 
     # Connect to arduino
     try:
@@ -37,75 +39,128 @@ def main():
     # Get the pic
     imagename = 'testimg.png'
     img = Image.open(imagename).convert('L') # greyscale
-    print 'IMAGE SIZE hor_size: ', img.size[0], 'vert_size: ', img.size[1]
-    max_size = 100, 100 # TODO change this
-    img.thumbnail(max_size, Image.ANTIALIAS)
-    hor_size, vert_size = img.size
-    print 'AFTER SCALING hor_size: ', hor_size, 'vert_size: ', vert_size
+    print 'IMAGE SIZE hor_pic_size: ', img.size[0], 'vert_pic_size: ', img.size[1]
+    max_pic_size = 100, 100
+    img.thumbnail(max_pic_size, Image.ANTIALIAS)
+    hor_pic_size, vert_pic_size = img.size
+    print 'AFTER SCALING hor_pic_size: ', hor_pic_size, 'vert_pic_size: ', vert_pic_size
 
-    # User inputs
-    cur_x = input('current x position in mm: ')
-    cur_y = -1*input('current y position in mm: ')
+    # User inputs, remember that y is -'ve in my coor system but +'ve in pixel space
+    start_x = input('current x position in mm: ')
+    start_y = -1*input('current y position in mm: ')
     end_x = input('end x position in mm: ')
     end_y = -1*input('end y position in mm: ')
     res = input('resolution in mm: ')
 
-    # These are the values which match up with hor_size, vert_size
-    x, y = 0, 0
+    # Does the vertical or horizontal dimension determine the picture scale?
+    dwg_ratio = vert_pic_size/hor_pic_size
+    board_ratio = abs( (end_y-start_y) / (end_x-start_x) )
+    if dwg_ratio > board_ratio:
+        x_dim_max = False
+        far_x = int(abs(end_y - start_y) / dwg_ratio)
+        top_y = end_y
+        print "Y dimension is max"
+    else:
+        x_dim_max = True
+        top_y = int((end_x - start_x) * dwg_ratio)
+        far_x = end_x
+        print "X dimension is max"
+
+    right = True
     up = True
-    while x < hor_size:
-        if up == True:
-            print 'going up'
-            while y < vert_size:
-                print x, y
-                # pixel density between 0 and 1
-                density = img.getpixel((x, vert_size - 1 - int(y)))/255
-                print 'den: ', density
-                max_len = (1 - density) * scale
-                if max_len < 0.5:
-                    len = 0.5
-                print 'len: ', len
-                if y * len > vert_size:
-                    len = vert_size - y - 1
-                    if len == 0:
-                        break
 
-                draw(2*scale,0)
-                draw(0,scale*len/2.0)
-                draw(-2*scale,0)
-                draw(0,scale*len/2.0)
+    cur_x = start_x
+    cur_y = start_y
 
-                y += len
-            draw(scale*2.2,0)
-            x += 2.2
-            print 'moving over'
-            print 'x, y: ', x, y
+    # Draw the picture
+    while cur_x < end_x:
+        # get the x, y location in pixel space
+        if x_dim_max:
+            x = int( ( (cur_x - start_x) / (end_x - start_x) ) * hor_pic_size )
+            x = max( min( x, far_x ), 0)
+            y = int( abs( ( (cur_y - start_y) / (top_y - start_y) ) * vert_pic_size) )
+            y = max( min( abs(y), top_y ), 0)
         else:
-            print 'going down'
-            while y > 0:
-                print x, y
-                density = img.getpixel((x, vert_size - 1 - int(y)))/300.0 + 0.1 # 0.1 so that it does move sometimes. 0 < density < 1
-                print 'den: ', density
-                len = (1 - density) * 7
-                if len < 0.5:
-                    len = 0.5
-                print 'len: ', len
-                if y - len < 0:
-                    len = y
-                    if len == 0:
-                        break
+            y = int( abs( ( (cur_y - start_y) / (end_y - start_y) ) * vert_pic_size) )
+            y = max( min( abs(y), top_y ), 0)
+            x = int( ( (cur_x - start_x) / (far_x - start_x) ) * hor_pic_size )
+            x = max( min( x, far_x ), 0)
+        print "at x,y: ", x, y
 
-                draw(2*scale,0)
-                draw(0,-scale*len/2.0)
-                draw(-scale*2,0)
-                draw(0,-scale*len/2.0)
+        # Find an assigned length based on the current location
+        # pixel density between 0 and 1
+        density = img.getpixel( (x, vert_pic_size - 1 - y) ) / 255
+        print "density: ", density
+        assigned_length = (1-density) * res
+        print "first assigned length: ", assigned_length
 
-                y -= len
-            draw(scale*2.2,0)
-            x += 2.2
-            print 'moving over'
-        up = not up
+        # Average values over the length of the assigned length
+        # what is the index of the y pixel at the end of the assigned length?
+        if up:
+            last_y = int( abs( ( (cur_y + assigned_length - start_y) / (top_y - start_y) ) * vert_pic_size) )
+        else:
+            last_y = int( abs( ( (cur_y - assigned_length - start_y) / (top_y - start_y) ) * vert_pic_size) )
+        last_y = max( min( abs(y), vert_pic_size ), 0)
+        print "first last y: ", last_y
+        sum = 0
+        for y_index in range( min(y,last_y), max(y,last_y) ):
+            print y_index
+            sum += img.getpixel( (x, vert_pic_size - 1 - y) ) / 255
+        if last_y != y:
+            avg = sum / abs(y - last_y)
+        else:
+            avg = density
+        print "average density: ", avg
+        assigned_length = (1-avg) * res
+        print "next assigned_length: ", assigned_length
 
+        # Check to see if the derivative exceeds a certain threshold and stop there
+        # TODO
+
+        # Minimum line length
+        assigned_length = max( assigned_length, res * VERT_RES )
+        print "final assigned length: ", assigned_length
+
+        # The maximum length is the edge of the drawing area
+        if up:
+            last_y = y + assigned_length
+        else:
+            last_y = y - assigned_length
+        last_y = max( min( abs(y), vert_pic_size ), 0)
+        assigned_length = abs(y - last_y)
+
+        # Draw the line
+        # over then up/down
+        if right:
+            print "over", res
+            #draw(res,0)
+        else:
+            print "over", -res
+            #draw(-res,0)
+        if up:
+            cur_y += assigned_length
+            #draw(0,assigned_length)
+            print "up", assigned_length
+        else:
+            cur_y -= assigned_length
+            #draw(0,-assigned_length)
+            print "down", -assigned_length
+        right = not right
+        
+        # Condition for top or bottom of the board
+        if last_y >= top_y or last_y <= start_y:
+            print "changing direction"
+            cur_x += HORIZONTAL_SPACE * res
+            if right:
+                print "right"
+                #draw( HORIZONTAL_SPACE * res, 0 )
+            else:
+                print "left"
+                #draw( (HORIZONTAL_SPACE-1) * res, 0 )
+            up = not up
+
+    print "ok, all done"
+        
 def draw(x,y):
     global cur_x, cur_y, ser
     #print '(', x, y, ')',
@@ -120,7 +175,7 @@ def draw(x,y):
         sys.exit()
     # Wait for the arduino to finish the line or break after a while
     dist = math.sqrt( x**2 + y**2 )
-    time.sleep(1.2*dist/SPEED)
+    time.sleep( 1.2 * dist/SPEED)
     
 
 if __name__ == '__main__':
