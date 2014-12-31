1b0ecba68c03a66ae3f1aab96f8ec6a63e3ed89f
isaacphi <isaac.phil@gmail.com>
2014-12-30 20:46:17 -0500

    updates

--- density.py
+++ density.py
@@ -37,7 +37,7 @@ def main():
         time.sleep(1)
 
     # Get the pic
-    imagename = 'testimg.png'
+    imagename = 'test.png'
     img = Image.open(imagename).convert('L') # greyscale
     print 'IMAGE SIZE hor_pic_size: ', img.size[0], 'vert_pic_size: ', img.size[1]
     max_pic_size = 100, 100
@@ -47,13 +47,12 @@ def main():
 
     # User inputs, remember that y is -'ve in my coor system but +'ve in pixel space
     start_x = input('current x position in mm: ')
-    start_y = -1*input('current y position in mm: ')
+    start_y = input('current y position in mm: ')
     end_x = input('end x position in mm: ')
-    end_y = -1*input('end y position in mm: ')
-    res = input('resolution in mm: ')
+    end_y = input('end y position in mm: ')
 
     # Does the vertical or horizontal dimension determine the picture scale?
-    dwg_ratio = vert_pic_size/hor_pic_size
+    dwg_ratio = vert_pic_size/float(hor_pic_size)
     board_ratio = abs( (end_y-start_y) / (end_x-start_x) )
     if dwg_ratio > board_ratio:
         x_dim_max = False
@@ -62,12 +61,15 @@ def main():
         print "Y dimension is max"
     else:
         x_dim_max = True
-        top_y = int((end_x - start_x) * dwg_ratio)
+        top_y = int(abs(end_x - start_x) * dwg_ratio)
         far_x = end_x
         print "X dimension is max"
 
     right = True
     up = True
+
+    min_length = math.ceil( abs(start_y-end_y) / vert_pic_size )
+    res = 1.2 * min_length
 
     cur_x = start_x
     cur_y = start_y
@@ -75,24 +77,22 @@ def main():
     # Draw the picture
     while cur_x < end_x:
         # get the x, y location in pixel space
+
+        print "cur_x, cur_y ", cur_x, cur_y
         if x_dim_max:
             x = int( ( (cur_x - start_x) / (end_x - start_x) ) * hor_pic_size )
-            x = max( min( x, far_x ), 0)
             y = int( abs( ( (cur_y - start_y) / (top_y - start_y) ) * vert_pic_size) )
-            y = max( min( abs(y), top_y ), 0)
         else:
             y = int( abs( ( (cur_y - start_y) / (end_y - start_y) ) * vert_pic_size) )
-            y = max( min( abs(y), top_y ), 0)
             x = int( ( (cur_x - start_x) / (far_x - start_x) ) * hor_pic_size )
-            x = max( min( x, far_x ), 0)
+        x = min( max(0, x), hor_pic_size-1 )
+        y = min( max(0, y), vert_pic_size-1 )
         print "at x,y: ", x, y
 
         # Find an assigned length based on the current location
         # pixel density between 0 and 1
-        density = img.getpixel( (x, vert_pic_size - 1 - y) ) / 255
-        print "density: ", density
+        density = img.getpixel( (x, vert_pic_size - 1 - y) ) / 255.0
         assigned_length = (1-density) * res
-        print "first assigned length: ", assigned_length
 
         # Average values over the length of the assigned length
         # what is the index of the y pixel at the end of the assigned length?
@@ -101,7 +101,6 @@ def main():
         else:
             last_y = int( abs( ( (cur_y - assigned_length - start_y) / (top_y - start_y) ) * vert_pic_size) )
         last_y = max( min( abs(y), vert_pic_size ), 0)
-        print "first last y: ", last_y
         sum = 0
         for y_index in range( min(y,last_y), max(y,last_y) ):
             print y_index
@@ -110,46 +109,47 @@ def main():
             avg = sum / abs(y - last_y)
         else:
             avg = density
-        print "average density: ", avg
         assigned_length = (1-avg) * res
-        print "next assigned_length: ", assigned_length
 
         # Check to see if the derivative exceeds a certain threshold and stop there
         # TODO
 
-        # Minimum line length
-        assigned_length = max( assigned_length, res * VERT_RES )
-        print "final assigned length: ", assigned_length
+        # Minimum line length is the physical length of one pixel
+        assigned_length = max( assigned_length, min_length )
 
         # The maximum length is the edge of the drawing area
         if up:
-            last_y = y + assigned_length
+            last_y = cur_y - assigned_length
         else:
-            last_y = y - assigned_length
-        last_y = max( min( abs(y), vert_pic_size ), 0)
-        assigned_length = abs(y - last_y)
+            last_y = cur_y + assigned_length
+        if last_y < min(start_y, end_y) or last_y > max(start_y, end_y):
+            if up:
+                assigned_length = abs(cur_y - min(start_y, end_y) )
+            else:
+                assigned_length = abs(cur_y - max(start_y, end_y) )
 
         # Draw the line
         # over then up/down
         if right:
-            print "over", res
-            #draw(res,0)
+            pass
+            draw(res,0)
         else:
-            print "over", -res
-            #draw(-res,0)
+            pass
+            draw(-res,0)
         if up:
-            cur_y += assigned_length
-            #draw(0,assigned_length)
-            print "up", assigned_length
-        else:
             cur_y -= assigned_length
-            #draw(0,-assigned_length)
-            print "down", -assigned_length
+            draw(0,assigned_length)
+        else:
+            cur_y += assigned_length
+            draw(0,-assigned_length)
         right = not right
+
+        print top_y
+        print dwg_ratio
+        print last_y
         
         # Condition for top or bottom of the board
-        if last_y >= top_y or last_y <= start_y:
-            print "changing direction"
+        if last_y <= top_y or last_y >= start_y:
             cur_x += HORIZONTAL_SPACE * res
             if right:
                 print "right"
@@ -170,9 +170,9 @@ def draw(x,y):
     # Update coordinates
     cur_x, cur_y = cur_x + x, cur_y + y
     # Warning if at edge of whiteboard
-    if cur_x > RIGHT_EDGE or cur_x < LEFT_EDGE or -1*cur_y > BOTTOM_EDGE or -1*cur_y < TOP_EDGE:
-        print "WARNING: edge of board at " + str(cur_x) + ',' + str(cur_y)
-        sys.exit()
+#    if cur_x > RIGHT_EDGE or cur_x < LEFT_EDGE or -1*cur_y > BOTTOM_EDGE or -1*cur_y < TOP_EDGE:
+#        print "WARNING: edge of board at " + str(cur_x) + ',' + str(cur_y)
+#        sys.exit()
     # Wait for the arduino to finish the line or break after a while
     dist = math.sqrt( x**2 + y**2 )
     time.sleep( 1.2 * dist/SPEED)
